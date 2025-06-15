c9b753418a15077a1190ba2043c85c2d4f9dc05b
bam <113142704+databaam9@users.noreply.github.com>
2025-06-14 22:41:39 -0400

    Add files via upload

---
+++ Untitled-3.py
@@ -0,0 +1,17 @@
+persona = {}
+print("hola")
+# Solicitar y agregar datos al diccionario
+persona['Nombre'] = input("Introduce tu nombre: ")
+print(persona)
+
+persona['Edad'] = input("Introduce tu edad: ")
+print(persona)
+
+persona['Sexo'] = input("Introduce tu sexo: ")
+print(persona)
+
+persona['Teléfono'] = input("Introduce tu número de teléfono: ")
+print(persona)
+
+persona['Correo electrónico'] = input("Introduce tu correo electrónico: ")
+print(persona)
